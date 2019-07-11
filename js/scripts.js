81f8cb8efc9755990469e00c8b3b4cf0bfeb70d4
Breken Davis and Mike McShane <mike@>
2019-07-10 20:07:06 -0700

    Added a second player.

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,23 @@ var diceArray = [1, 2, 3, 4, 5, 6];
 var currentRoll = [];
 var scoreTotal = 0;
 var rollTotal = 0;
+
 //UI Logic
 
 $(document).ready(function() {
-  $(".rollbutton").click(function(event) {
+  $(".rollbutton1").click(function(event) {
     event.preventDefault();
     $('ul').text('');
     var roll = diceArray[Math.floor(Math.random()*diceArray.length)];
 
     rollTotal += roll;
-    $("#roll-total").text(rollTotal);
+    $("#roll-total1").text(rollTotal);
 
     if (roll === 1) {
       currentRoll = [];
       $('ul').text();
       rollTotal = 0;
-      $("#roll-total").empty();
+      $("#roll-total1").empty();
       alert("Your turn has ended!")
     } else {
       currentRoll.push(roll);
@@ -27,26 +28,26 @@ $(document).ready(function() {
       currentRoll = [];
       $('ul').text();
       rollTotal = 0;
-      $("#roll-total").empty();
+      $("#roll-total1").empty();
       scoreTotal = 0;
-      $("#score-total").empty();
+      $("#score-total1").empty();
     }
     console.log(currentRoll);
 
     for (var i =0; i < currentRoll.length; i += 1) {
-      $("#list").append("<li>" + currentRoll[i] + "</li>")
+      $("#list1").append("<li>" + currentRoll[i] + "</li>")
     }
   });
 
-  $(".holdbutton").click(function(event) {
+  $(".holdbutton1").click(function(event) {
     event.preventDefault();
 
-    $("#roll-total").empty();
+    $("#roll-total1").empty();
 
     currentRoll = [];
 
     scoreTotal += rollTotal;
-    $("#score-total").text(scoreTotal);
+    $("#score-total1").text(scoreTotal);
     rollTotal = 0;
 
     if (scoreTotal >= 100) {
@@ -54,9 +55,67 @@ $(document).ready(function() {
       currentRoll = [];
       $('ul').text();
       rollTotal = 0;
-      $("#roll-total").empty();
+      $("#roll-total1").empty();
       scoreTotal = 0;
-      $("#score-total").empty();
+      $("#score-total1").empty();
+    }
+
+  console.log(rollTotal);
+});
+});
+
+$(document).ready(function() {
+  $(".rollbutton2").click(function(event) {
+    event.preventDefault();
+    $('ul').text('');
+    var roll = diceArray[Math.floor(Math.random()*diceArray.length)];
+
+    rollTotal += roll;
+    $("#roll-total2").text(rollTotal);
+
+    if (roll === 1) {
+      currentRoll = [];
+      $('ul').text();
+      rollTotal = 0;
+      $("#roll-total2").empty();
+      alert("Your turn has ended!")
+    } else {
+      currentRoll.push(roll);
+    }
+    if (scoreTotal >= 100) {
+      currentRoll = [];
+      $('ul').text();
+      rollTotal = 0;
+      $("#roll-total2").empty();
+      scoreTotal = 0;
+      $("#score-total2").empty();
+    }
+    console.log(currentRoll);
+
+    for (var i =0; i < currentRoll.length; i += 1) {
+      $("#list2").append("<li>" + currentRoll[i] + "</li>")
+    }
+  });
+
+  $(".holdbutton2").click(function(event) {
+    event.preventDefault();
+
+    $("#roll-total2").empty();
+
+    currentRoll = [];
+
+    scoreTotal += rollTotal;
+    $("#score-total2").text(scoreTotal);
+    rollTotal = 0;
+
+    if (scoreTotal >= 100) {
+      alert("Congratulations! You win the game!")
+      currentRoll = [];
+      $('ul').text();
+      rollTotal = 0;
+      $("#roll-total2").empty();
+      scoreTotal = 0;
+      $("#score-total2").empty();
     }
 
   console.log(rollTotal);
