105e552e39b2db5516ec026dee5edeb11e62a7b5
Breken Davis and Mike McShane <mike@>
2019-07-10 19:10:57 -0700

    incorporated the add function with summand and pushed result to the DOM. Currently not adding correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Business Logic
 var diceArray = [1, 2, 3, 4, 5, 6];
 var currentRoll = [];
-
+var rollTotal = 0;
 //UI Logic
 
 $(document).ready(function() {
@@ -10,12 +10,14 @@ $(document).ready(function() {
     $('ul').text('');
     var roll = diceArray[Math.floor(Math.random()*diceArray.length)];
 
-      if (roll === 1) {
-        currentRoll = [];
-        $('ul').text('');
-      } else {
-        currentRoll.push(roll);
-      }
+
+    if (roll === 1) {
+      currentRoll = [];
+      $('ul').text('');
+      rollTotal = 0;
+    } else {
+      currentRoll.push(roll);
+    }
     console.log(currentRoll);
 
     for (var i =0; i < currentRoll.length; i += 1) {
@@ -23,4 +25,15 @@ $(document).ready(function() {
     }
   });
 
+  $(".holdbutton").click(function(event) {
+    event.preventDefault();
+    $("#roll-total").empty()
+    currentRoll.forEach(function(summand) {
+      rollTotal += summand;
+      $("#roll-total").text(rollTotal);
+    });
+
+
+  console.log(rollTotal);
+});
 });
